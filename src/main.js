33fd2cbd24ab64cd64986194a2fc60b6d7f526bd
Francisco Bento <franciscobdsjunior@gmail.com>
2022-08-28 22:11:06 -0300

    Remove unused 'semantic-ui-vue' from main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import "semantic-ui-css/semantic.min.css";
 
-import * as SuiVue from "semantic-ui-vue";
 import * as Vue from "vue";
 
 import App from "./App.vue";
 
 let app = Vue.createApp(App);
-
-app.use(SuiVue);
 
 app.mount("#app");
